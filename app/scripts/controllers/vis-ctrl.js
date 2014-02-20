bb5e5d449797d29046629c842b542d47d6eea22d
Ryan on Ubuntu <yulinye.yy@gmail.com>
2014-02-20 00:07:31 -0800

    numeric column filter for axis selection control

--- app/scripts/controllers/vis-ctrl.js
+++ app/scripts/controllers/vis-ctrl.js
@@ -3,186 +3,200 @@
 angular.module('controllers', ['dataManager', 'underscore', 'ngDropdowns'])
 
 .controller('tabsCtrl', ['$scope', '$log',
-    function($scope) {
-        $scope.tabs = [{
-            title: 'Table View',
-            imgUrl: '/images/icons/grid-26.png',
-            contentUrl: '/views/tab-content-grid.html',
-            active: true
-        }, {
-            title: 'Scatter Plot',
-            imgUrl: '/images/icons/scatter_plot-25.png',
-            contentUrl: '/views/tab-content-scatter.html',
-            active: false
-        }, {
-            title: 'Stock Trend',
-            imgUrl: '/images/icons/scatter_plot-25.png',
-            contentUrl: '/views/tab-content-stock-trend.html',
-            active: false
-        }];
-    }
+  function($scope) {
+    $scope.tabs = [{
+      title: 'Table View',
+      imgUrl: '/images/icons/grid-26.png',
+      contentUrl: '/views/tab-content-grid.html',
+      active: true
+    }, {
+      title: 'Scatter Plot',
+      imgUrl: '/images/icons/scatter_plot-25.png',
+      contentUrl: '/views/tab-content-scatter.html',
+      active: false
+    }, {
+      title: 'Stock Trend',
+      imgUrl: '/images/icons/scatter_plot-25.png',
+      contentUrl: '/views/tab-content-stock-trend.html',
+      active: false
+    }];
+  }
 ])
 
 .controller('scatterCtrl', ['$scope', '$log', '_', 'dataLoader', 'dataStore', 'dataTransformer',
+  function($scope, $log, _, dataLoader, dataStore, dataTransformer) {
+    $scope.showPlaceholder = true;
+    $scope.showError = false;
+    //prepare the data
+    var rawData = dataLoader.get({
+        data: 'testdata'
+      }, function(rawData) {
+        //callback function from the query
+
+        $scope.columns = rawData.input.columns;
+        $scope.attrOptions = [];
+        $scope.attrForX = {
+          attrName: 'Please select',
+          value: false
+        };
+        $scope.attrForY = {
+          attrName: 'Please select',
+          value: false
+        };
+
+        _.each($scope.columns, function(element) {
+          $scope.attrOptions.push({
+            attrName: element,
+            value: element
+          });
+        });
+        $scope.showPlaceholder = false;
+      },
+      function(reason) {
+        $scope.showError = true;
+        $log.log('data loading failed. reason: ' + reason);
+      });
+
+    //update chart data when user change the selection
+    $scope.axisChange = function() {
+      if ($scope.attrForX.value && $scope.attrForY.value) {
+        // $scope.chart=null;
+        $scope.data = dataTransformer.LRTonvd3Scatter(rawData, $scope.attrForX.value, $scope.attrForY.value, '');
+      }
+    };
+
+    //for stock prediction only
+    $scope.getShape = function() {
+      return function(d) {
+        return d.shape;
+      };
+    };
+
+    //configure the chart
+    $scope.axistickformatFunction = function() {
+      return d3.format('.1f');
+    };
+
+    $scope.callbackFunction = function() {
+      return function(chart) {
+        $scope.chart = chart;
+      };
+    };
+
+
+  }
+])
+  .controller('stockCtrl', ['$scope', '$log', '_', 'dataLoader', 'dataStore', 'dataTransformer',
     function($scope, $log, _, dataLoader, dataStore, dataTransformer) {
-        $scope.showPlaceholder = true;
-        $scope.showError = false;
-        //prepare the data
-        var rawData = dataLoader.get({
-                data: 'testdata'
-            }, function(rawData) {
-                //callback function from the query
+      $scope.showPlaceholder = true;
+      $scope.showError = false;
 
-                $scope.columns = rawData.input.columns;
-                $scope.attrOptions = [];
-                $scope.attrForX = {
-                    attrName: 'Please select',
-                    value: false
-                };
-                $scope.attrForY = {
-                    attrName: 'Please select',
-                    value: false
-                };
+      //prepare the data
+      var rawData = dataLoader.get({
+          data: 'PAL.stock_1d'
+        }, function(rawData) {
+          //callback function from the query
+          $scope.columns = dataTransformer.getNumericColomns(rawData.columns, rawData.values);
+          $scope.attrOptions = [];
+          $scope.attrForX = {
+            attrName: 'Please select',
+            value: false
+          };
+          $scope.attrForY = {
+            attrName: 'Please select',
+            value: false
+          };
 
-                _.each($scope.columns, function(element) {
-                    $scope.attrOptions.push({
-                        attrName: element,
-                        value: element
-                    });
-                });
-                $scope.showPlaceholder = false;
-            },
-            function(reason) {
-                $scope.showError = true;
-                $log.log('data loading failed. reason: ' + reason);
+          _.each($scope.columns, function(element) {
+            $scope.attrOptions.push({
+              attrName: element,
+              value: element
             });
+          });
+          $scope.attrOptions.push({
+            attrName: 'DATE',
+            value: 'DATE'
+          });
+          //render a defaul chart before selecting axis
+          // $scope.data = dataTransformer.DTToStockPredition(rawData, 'DATE', 'STOCK_CLOSE_PRICE', 'predicted');
 
-        //update chart data when user change the selection
-        $scope.axisChange = function(axisName) {
-            if ($scope.attrForX.value && $scope.attrForY.value) {
-                // $scope.chart=null;
-                $scope.data = dataTransformer.LRTonvd3Scatter(rawData, $scope.attrForX.value, $scope.attrForY.value, '');
-            }
+          $scope.showPlaceholder = false;
+        },
+        function(reason) {
+          //data loading failed
+          $scope.showError = true;
+          $log.log('data loading failed. reason: ' + reason);
+        });
+
+      //update chart data when user change the selection
+      $scope.axisChange = function() {
+        if ($scope.attrForX.value && $scope.attrForY.value) {
+          // $scope.chart=null;
+          $scope.data = dataTransformer.DTToStockPredition(rawData, $scope.attrForX.value, $scope.attrForY.value, 'predicted');
+        }
+      };
+
+      //for stock prediction only
+      $scope.getShape = function() {
+        return function(d) {
+          return d.shape;
         };
+      };
 
-        //for stock prediction only
-        $scope.getShape = function() {
-            return function(d) {
-                return d.shape;
-            };
+      //configure the chart
+      $scope.axistickformatFunction = function() {
+        var dateFormatter = function(d) {
+          return d3.time.format('%m/%d/%Y')(new Date(d));
         };
+        var numFormatter = d3.format('.1f');
 
-        //configure the chart
-        $scope.axistickformatFunction = function() {
-            return d3.format('.1f');
-        };
-
-        $scope.callbackFunction = function() {
-            return function(chart) {
-                $scope.chart = chart;
-            };
-        };
-
+        var formatter = $scope.attrForX.value === 'DATE' ? dateFormatter : numFormatter;
+        return formatter;
+      };
 
     }
-])
-    .controller('stockCtrl', ['$scope', '$log', '_', 'dataLoader', 'dataStore', 'dataTransformer',
-        function($scope, $log, _, dataLoader, dataStore, dataTransformer) {
-            $scope.showPlaceholder = true;
-            $scope.showError = false;
+  ])
+  .controller('gridCtrl', ['_', '$scope', '$log', 'dataLoader', 'dataStore', 'dataTransformer',
+    function(_, $scope, $log, dataLoader, dataStore, dataTransformer) {
+      $scope.showPlaceholder = true;
+      $scope.showError = false;
 
-            //prepare the data
-            var rawData = dataLoader.get({
-                    data: 'PAL.stock_1d'
-                }, function(rawData) {
-                    //callback function from the query
-                    $scope.columns = rawData.columns;
-                    $scope.data = dataTransformer.DTToStockPredition(rawData, 'DATE', 'STOCK_CLOSE_PRICE', 'predicted');
+      // initiate grid option. Skipping initiation will invoke compiling error!
+      $scope.gridData = [];
+      $scope.columnDefs = [];
+      $scope.gridOptions = {
+        data: 'gridData',
+        enablePinning: true,
+        // showGroupPanel: true,         ---> buggy in 2.0.7, will be fixed in 2.0.8
+        enableColumnResize: true,
+        columnDefs: 'columnDefs'
+      };
 
-                    $scope.showPlaceholder = false;
-                },
-                function(reason) {
-                    //data loading failed
-                    $scope.showError = true;
-                    $log.log('data loading failed. reason: ' + reason);
-                });
-
-            //for stock prediction only
-            $scope.getShape = function() {
-                return function(d) {
-                    return d.shape;
-                };
+      var columns = [];
+      dataLoader.get({
+          data: 'testdata'
+        }, function(rawData) {
+          $scope.columns = rawData.input.columns;
+          _.each($scope.columns, function(element) {
+            var column = {
+              field: element
             };
+            columns.push(column);
+          });
+          //append the $group to indicate input data or predicted data
+          columns.push({
+            field: '$group',
+            displayName: 'Group'
+          });
+          $scope.columnDefs = columns;
+          $scope.gridData = dataTransformer.LRToGrid(rawData);
 
-            //configure the chart
-            $scope.dateAxistickformatFunction = function() {
-                return function(d) {
-                    return d3.time.format('%m/%d/%Y')(new Date(d));
-                }
-            };
+          $scope.showPlaceholder = false;
+        },
+        function(reason) {
+          $scope.showError = true;
+          $log.log('data loading failed. reason: ' + reason);
+        }); //rawData callback
 
-            $scope.priceAxistickformatFunction = function() {
-                return d3.format('.1f');
-            };
-
-            $scope.tooltipContentFunction = function(){
-                return function(key, x){
-                    //$log.log(key);
-                    return '<strong> ho! </strong>'; 
-                }
-            }
-
-
-            $scope.callbackFunction = function() {
-                return function(chart) {
-                    // $log.log(chart);
-                };
-            };
-
-
-        }
-    ])
-    .controller('gridCtrl', ['$scope', '$log', 'dataLoader', 'dataStore', 'dataTransformer',
-        function($scope, $log, dataLoader, dataStore, dataTransformer) {
-            $scope.showPlaceholder = true;
-            $scope.showError = false;
-
-            // initiate grid option. Skipping initiation will invoke compiling error!
-            $scope.gridData = [];
-            $scope.columnDefs = [];
-            $scope.gridOptions = {
-                data: 'gridData',
-                enablePinning: true,
-                // showGroupPanel: true,         ---> buggy in 2.0.7, will be fixed in 2.0.8
-                enableColumnResize: true,
-                columnDefs: 'columnDefs'
-            };
-
-            var columns = [];
-            var rawData = dataLoader.get({
-                    data: 'testdata'
-                }, function(rawData) {
-                    $scope.columns = rawData.input.columns;
-                    _.each($scope.columns, function(element) {
-                        var column = {
-                            field: element
-                        };
-                        columns.push(column);
-                    });
-                    //append the $group to indicate input data or predicted data
-                    columns.push({
-                        field: '$group',
-                        displayName: 'Group'
-                    });
-                    $scope.columnDefs = columns;
-                    $scope.gridData = dataTransformer.LRToGrid(rawData);
-
-                    $scope.showPlaceholder = false;
-                },
-                function(reason) {
-                    $scope.showError = true;
-                    $log.log("data loading failed. reason: " + reason);
-                }); //rawData callback
-
-        }
-    ]);
+    }
+  ]);
--- app/scripts/directives/vis-directives.js
+++ app/scripts/directives/vis-directives.js
@@ -1,24 +1,24 @@
 'use strict';
 
 angular.module('visDirectives', [])
-    .directive('visScatter', function() {
-        return {
-            restrict: 'EA',
-            templateUrl: '/views/vis-scatter-template.html',
-            scope: true
-        };
-    })
-    .directive('visStockTrend', function() {
-        return {
-            restrict: 'EA',
-            templateUrl: '/views/vis-stock-template.html',
-            scope: true
-        };
-    })
-    .directive('visGrid', function() {
-        return {
-            restrict: 'EA',
-            templateUrl: '/views/vis-grid-template.html',
-            scope: true
-        };
-    });
+  .directive('visScatter', function() {
+    return {
+      restrict: 'EA',
+      templateUrl: '/views/vis-scatter-template.html',
+      scope: true
+    };
+  })
+  .directive('visStockTrend', function() {
+    return {
+      restrict: 'EA',
+      templateUrl: '/views/vis-stock-template.html',
+      scope: true
+    };
+  })
+  .directive('visGrid', function() {
+    return {
+      restrict: 'EA',
+      templateUrl: '/views/vis-grid-template.html',
+      scope: true
+    };
+  });
